951bb1c0113f08e9aff6146c3838febb52c65405
Brent Bernasol <77885278+brentbits@users.noreply.github.com>
2022-06-15 00:40:32 +0800

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
-import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import { HashRouter, Link, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
@@ -10,19 +10,19 @@ import Login from './components/Login';
 function App() {
   return (
     <div>
-          <Router>
+          <HashRouter>
               <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                <Navbar.Brand href="/">MyDiary-app</Navbar.Brand>
+                <Navbar.Brand href="/#/">MyDiary-app</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                   <Nav className="me-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
-                    <Nav.Link href="/about">About us</Nav.Link>
+                    <Nav.Link href="/#/home">Home</Nav.Link>
+                    <Nav.Link href="/#/about">About us</Nav.Link>
                     <NavDropdown title="Profile" id="collasible-nav-dropdown">
                       <NavDropdown.Item href="/account">Account</NavDropdown.Item>
                       <NavDropdown.Divider />
-                      <NavDropdown.Item href="/login">Logout</NavDropdown.Item>
+                      <NavDropdown.Item href="/#/login">Logout</NavDropdown.Item>
                     </NavDropdown>
                   </Nav>
                   <Nav>
@@ -40,7 +40,7 @@ function App() {
                   <Route exact path="/login" element={<Login />} />
                   <Route exact path="/" element={<Home />} />
                 </Routes>
-          </Router>
+          </HashRouter>
     </div>
   );
 }
